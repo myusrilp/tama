01b9cb1f212f31a395320fb05036ee51ec7697fc
alfarisin <alfarisi686@gmail.com>
2023-04-15 23:16:25 +0700

    Commit --progress benda latihan

--- app/src/main/java/com/example/gameedukasi/game/ActivityBendaLatihan.java
+++ app/src/main/java/com/example/gameedukasi/game/ActivityBendaLatihan.java
@@ -1,6 +1,7 @@
 package com.example.gameedukasi.game;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import android.widget.Button;
 
 import com.example.gameedukasi.R;
+import com.example.gameedukasi.adapter.AdapterBendaList;
 import com.example.gameedukasi.model.ModelBenda;
 
 import java.util.ArrayList;
@@ -17,6 +19,7 @@ public class ActivityBendaLatihan extends AppCompatActivity {
 
     private Context context = ActivityBendaLatihan.this;
     List<ModelBenda> bendas;
+    AdapterBendaList adapterBendaList;
 
     private RecyclerView rvBendaList;
     private Button back;
@@ -26,7 +29,21 @@ public class ActivityBendaLatihan extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_benda_latihan);
 
+        rvBendaList = (RecyclerView) findViewById(R.id.rvBendaList);
+        back = (Button) findViewById(R.id.back);
+
         bendas = new ArrayList<>();
-        bendas.add(new ModelBenda(""));
+        bendas.add(new ModelBenda("jam", R.drawable.jam));
+        bendas.add(new ModelBenda("papantulis", R.drawable.papantulis));
+        bendas.add(new ModelBenda("buku", R.drawable.buku));
+        bendas.add(new ModelBenda("tas", R.drawable.tas));
+
+        initiateRecyclerView();
+    }
+
+    private void initiateRecyclerView(){
+        adapterBendaList = new AdapterBendaList(context, bendas);
+        rvBendaList.setLayoutManager(new GridLayoutManager(context, 3));
+        rvBendaList.setAdapter(adapterBendaList);
     }
 }
